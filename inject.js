570d55d93c797cc9b64424ea986ac01469cf64ff
orlandoperflo <69471535+orlandoperflo@users.noreply.github.com>
2025-11-05 10:58:51 -0600

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,31 @@
+// Inject favicon into every page
+(function() {
+  const link = document.createElement("link");
+  link.rel = "icon";
+  link.type = "image/webp";
+  link.href = "https://plusbrand.com/wp-content/uploads/2025/11/ALETHRA_Logo-scaled.webp";
+  document.head.appendChild(link);
+})();
+
+// NAV
+fetch("/nav.html")
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("nav-placeholder").innerHTML = html;
+    initNavLogic();
+  });
+
+  //NAV MOBIL
+fetch("/nav-mobil.html")
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("nav-mobil-placeholder").innerHTML = html;
+    initNavLogic();
+  });
+
+// FOOTER
+fetch("/footer.html")
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("footer-placeholder").innerHTML = html;
+  });
